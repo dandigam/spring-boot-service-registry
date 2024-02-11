2ee2152771230054c31489c56d3844d50639c19e
venkat <dandigam@gmail.com>
2024-02-11 08:42:44 +0530

    created new spring boot application and service registry

---
+++ service-registry/src/test/java/com/dandigam/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dandigam.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
